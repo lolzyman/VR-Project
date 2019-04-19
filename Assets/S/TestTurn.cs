da32115a4e651811896aecdc1ca47abe46a61054
Mark Kent <kmark@inside.andrews.edu>
2019-04-19 16:53:50 -0400

    Got head turning with joystick working - Noah

---
+++ Assets/S/TestTurn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+namespace Valve.VR.InteractionSystem {
+
+public class TestTurn : MonoBehaviour
+{
+   public SteamVR_Action_Vector2 TurnAround;
+   public SteamVR_Input_Sources thisHand;
+   public GameObject targetTransform;
+
+        public float speed = 5.0f;
+
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+            //Vector3  targetPosition = new Vector3(0, TurnAround.GetAxis(thisHand).x, 0);
+            //transform.Rotate(targetPosition);
+
+            targetTransform.transform.Rotate(0.0f, TurnAround.GetAxis(thisHand).x * speed * Time.deltaTime, 0.0f);
+    }
+}
+}
---
+++ Assets/S/TestWalk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+namespace Valve.VR.InteractionSystem{
+
+
+public class TestWalk : MonoBehaviour
+{
+	public SteamVR_Action_Boolean StupidButton;
+	public SteamVR_Action_Vector2 WalkAround;
+	public SteamVR_Input_Sources thisHand;
+	public GameObject targetTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(StupidButton.GetStateUp(thisHand)){
+			Debug.Log("AAHHHHH!!!!");
+		}
+		Debug.Log(WalkAround.GetAxis(thisHand));
+		targetTransform.transform.position += targetTransform.transform.forward * Time.deltaTime*WalkAround.GetAxis(thisHand).y;
+    }
+}
+}
+
